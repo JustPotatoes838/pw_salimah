fe01e023e2a77779da7ae8863ac6b03a8b045b5b
shofiyah2022-cpu <shofiyah2022@gmail.com>
2026-05-09 22:45:39 +0700

    Menambahkan frontend landing page Salimah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,117 +10,257 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-
     <header class="hero">
-
-    <nav class="navbar">
-        <div class="logo-wrap">
-            <img src="https://th.bing.com/th/id/R.95983909802defd30381d6b50060ff9a?rik=R%2bXKFFXHJTUMng&riu=http%3a%2f%2f3.bp.blogspot.com%2f-UBh4T8jZDe0%2fTvx7q49SNjI%2fAAAAAAAAACE%2fyYbRy0lvoTc%2fs1600%2fLogo%2bbaru%2bsalimah.jpg&ehk=ZjXEWxAXKLzzfKmAO99f71fB39FIpuhIO7%2fHIK12fuA%3d&risl=&pid=ImgRaw&r=0" alt="Salimah Maluku">
-            <div>
-                <h3>Salimah Maluku</h3>
-                <p>DPW Maluku</p>
-            </div>
-        </div>
-
-        <ul class="menu">
-            <li><a href="#">Beranda</a></li>
-            <li><a href="#">Profil</a></li>
-            <li><a href="#">Program</a></li>
-            <li><a href="#">Berita</a></li>
-            <li><a href="#">Kontak</a></li>
-        </ul>
-
-        <a href="#" class="btn-nav">Gabung Relawan</a>
-    </nav>
-
-    <section class="hero-content">
-
-        <div class="hero-left">
-            <span class="badge">Portal Resmi Salimah Maluku</span>
-
-            <h1>
-                Membina Perempuan,<br>
-                Menguatkan Keluarga
-            </h1>
-
-            <p>
-                Bersama Salimah Maluku membangun masyarakat
-                melalui dakwah, pendidikan, sosial, dan pemberdayaan.
-            </p>
-
-            <div class="hero-buttons">
-                <a href="#" class="btn-primary">Program Kami</a>
-                <a href="#" class="btn-outline">Lihat Berita</a>
-            </div>
-
-            <div class="stats">
+        <nav class="navbar">
+            <div class="logo-wrap">
+                <img src="https://th.bing.com/th/id/R.95983909802defd30381d6b50060ff9a?rik=R%2bXKFFXHJTUMng&riu=http%3a%2f%2f3.bp.blogspot.com%2f-UBh4T8jZDe0%2fTvx7q49SNjI%2fAAAAAAAAACE%2fyYbRy0lvoTc%2fs1600%2fLogo%2bbaru%2bsalimah.jpg&ehk=ZjXEWxAXKLzzfKmAO99f71fB39FIpuhIO7%2fHIK12fuA%3d&risl=&pid=ImgRaw&r=0" alt="Salimah Maluku">
                 <div>
-                <h2>50+</h2>
-                <span>Program</span>
+                    <h3>Salimah Maluku</h3>
+                    <p>DPW Maluku</p>
+                </div>
             </div>
 
-            <div>
-                <h2>1000+</h2>
-                <span>Relawan</span>
+            <ul class="menu">
+                <li><a href="#">Beranda</a></li>
+                <li><a href="#">Profil</a></li>
+                <li><a href="#">Program</a></li>
+                <li><a href="#">Berita</a></li>
+                <li><a href="#">Kontak</a></li>
+            </ul>
+
+            <a href="#" class="btn-nav">Admin</a>
+        </nav>
+        <section class="Konten-Utama">
+            <div class="main-text">
+        
+                <h1>
+                    Salimah Maluku<br>
+                </h1>
+
+                <p>
+                    Bersama Salimah Maluku membangun masyarakat
+                    melalui dakwah, pendidikan, sosial, dan pemberdayaan.
+                </p>
             </div>
-
-            <div>
-                <h2>20+</h2>
-                <span>Cabang</span>
-            </div>
-        </div>
-
-        </div>
-
-        <div class="hero-right">
-            <div class="glass-card main-card">
-                <h3>Program Unggulan</h3>
-                <div class="mini-grid">
-                <div class="mini-card">Pendidikan</div>
-                <div class="mini-card">Dakwah</div>
-                <div class="mini-card">Sosial</div>
-                <div class="mini-card">Keluarga</div>
-            </div>
-        </div>
-
-        <div class="floating-card top-card">
-            Berita & Kegiatan Aktif
-        </div>
-
-        <div class="floating-card bottom-card">
-            Portal Organisasi Modern
-        </div>
-
-        </div>
-
-    </section>
+        </section>
     </header>
+    
 
-    <section class="quick-services">
-        <h2>Layanan Cepat</h2>
+    
+        <section class="profil">
+            <div class="profil-header">
+                <h1>Salimah Maluku</h1>
+            </div>
+            <div class="profil-content">
+                <img class="profil-img-right" src="https://www.saburomedia.com/wp-content/uploads/2023/11/397539670_352485387352942_6984640641057299671_n.jpg" alt="Kegiatan Salimah Maluku">
+                <p>Persaudaraan Muslimah (Salimah) adalah organisasi kemasyarakatan perempuan yang didirikan pada 8 Maret 2000 oleh para aktivis dakwah perempuan sebagai wadah pemberdayaan muslimah Indonesia. Salimah hadir dengan kepedulian terhadap kondisi perempuan, keluarga, dan masyarakat melalui berbagai program sosial, pendidikan, dakwah, serta perlindungan anak guna membentuk perempuan yang cerdas, mandiri, dan bermanfaat bagi lingkungan sekitar.</p><br>
+                <p>Salimah Maluku merupakan bagian dari jaringan Salimah di tingkat daerah yang berkomitmen menjalankan visi organisasi dalam membangun masyarakat yang harmonis dan berdaya. Melalui berbagai kegiatan sosial, pembinaan, dan pemberdayaan perempuan, Salimah Maluku terus berupaya memberikan kontribusi positif bagi perkembangan perempuan, keluarga, dan masyarakat di wilayah Maluku.</p>
+            </div>
+        </section>
 
-        <div class="service-grid">
-            <div class="service-card">
-                <h3>Program Kerja</h3>
-                <p>Informasi kegiatan organisasi.</p>
+        <section class="visi-misi">
+            <div class="visi-misi-header">
+                <h1>Visi dan Misi Salimah Maluku</h1>
             </div>
 
-            <div class="service-card">
-                <h3>Berita</h3>
-                <p>Publikasi terbaru dan agenda.</p>
+            <div class="visi-misi-container">
+                <!-- Card Visi -->
+                <div class="card-visi">
+                    <h2>Visi</h2>
+                    <p>Menjadi Ormas Perempuan Pelopor Yang Berkontribusi Aktif dalam Meningkatkan Kualitas Hidup Perempuan, Anak dan Keluarga Indonesia.</p>
+                </div>
+
+                <!-- Card Misi -->
+                <div class="card-misi">
+                    <h2>Misi</h2>
+                    <ul>
+                        <li>Memperluas dan memperkokoh soliditas struktur di semua tingkatan.</li>
+                        <li>Meningkatkan kuantitas anggota sebagai massa Salimah.</li>
+                        <li>Meningkatkan kapasitas, kompetensi, dan integritas pengurus.</li>
+                        <li>Merealisasikan program yang terkait isu Perempuan, Anak, dan Keluarga.</li>
+                        <li>Meningkatkan peran serta dan kontribusi Salimah dalam berbagai bidang termasuk Dakwah, Pendidikan, Ekonomi, dan Sosial.</li>
+                        <li>Membangun dan memperkuat jaringan Kerjasama dengan berbagai pihak baik internal maupun eksternal.</li>
+                        <li>Mengembangkan sistem informasi dan komunikasi yang efektif untuk mendukung kegiatan organisasi.</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+        <section class="struktur-organisasi">
+            <h1>Struktur Kepengurusan Wilayah</h1>
+            <div class="org-wrapper">
+                
+                
+                <div class="org-chart">
+
+                    <!-- Ketua -->
+                    <div class="top-box">
+                        <div class="card">
+                            <div class="title">Ketua Umum</div>
+                            <div class="content">
+                                SANIAH, M.Pd
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="vertical-line"></div>
+
+                    <!-- Sekretaris & Bendahara -->
+                    <div class="middle-row">
+                        <div class="card">
+                            <div class="title">Sekretaris</div>
+                            <div class="content">
+                                Fitria Aras, S.Pd
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="title">Bendahara</div>
+                            <div class="content">
+                                Yunita Ismawati Tuasamu, M.Pd
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="vertical-line"></div>
+
+                    <!-- Departemen -->
+                    <div class="bottom-row">
+
+                        <div class="card dept">
+                            <div class="title">Departemen Dakwah</div>
+                            <div class="content">
+                                Salma, S.Pd (Ketua)<br>
+                                Dra Sri Hartati (Anggota)<br>
+                                Dr Maimunah, M.A (Anggota)
+                            </div>
+                        </div>
+
+                        <div class="card dept">
+                            <div class="title">Departemen Pendidikan dan Pelatihan</div>
+                            <div class="content">
+                                Saniah, M.Pd (Ketua)<br>
+                                Widya F Bakir, M.Pd (Anggota)
+                            </div>
+                        </div>
+
+                        <div class="card dept">
+                            <div class="title">Departemen Pengembangan Daerah dan SDM</div>
+                            <div class="content">
+                                Kamala Soleman, M.Si (Ketua)<br>
+                                Fitriyani Kaliki M.Sc (Anggota)<br>
+                                Nani Sangkala, M.Si (Anggota)
+                            </div>
+                        </div>
+
+                        <div class="card dept">
+                            <div class="title">Departemen Media dan Humas</div>
+                            <div class="content">
+                                Fitri Aras, S.Pd (Ketua)<br>
+                                Susanti Balami, S.PdI (Anggota)<br>
+                                Sumayya Ohoirat (Anggota)
+                            </div>
+                        </div>
+
+                        <div class="card dept">
+                            <div class="title">Departemen Ekonomi</div>
+                            <div class="content">
+                                Maryam Usemahu, S.Pd (Ketua)<br>
+                                Maya S.Kom (Anggota)<br>
+                                Tuti Uyara, S.Si (Anggota)
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="program">
+    
+            <div class="program-wrapper">
+
+                <div class="program-header">
+                    <h1>7 Program Unggulan <br> Salimah Maluku</h1>
+                </div>
+
+                <div class="program-container">
+
+                    <div class="program-card">
+                        <h2>Kossuma</h2>
+                        <p>Koperasi Serba Usaha Salimah.</p>
+                    </div>
+
+                    <div class="program-card">
+                        <h2>Komunitas</h2>
+                        <p>Sabila, KEP, KPSPL, KOB.</p>
+                    </div>
+
+                    <div class="program-card">
+                        <h2>Forsil MT</h2>
+                        <p>Forum Silaturahmi Majelis Ta'lim.</p>
+                    </div>
+
+                    <div class="program-card">
+                        <h2>IMSI</h2>
+                        <p>Ikatan Mubalighoh Salimah Indonesia.</p>
+                    </div>
+
+                    <div class="program-card">
+                        <h2>Rumah Pendidikan Salimah</h2>
+                        <p>PAUD, TKIT, P2AYD, SERASI, BQS, Sekolah Lansia.</p>
+                    </div>
+
+                    <div class="program-card">
+                        <h2>P2MB</h2>
+                        <p>Pos Penanggulangan Musibah dan Bencana.</p>
+                    </div>
+
+                    <div class="program-card">
+                        <h2>PKPS</h2>
+                        <p>Pelatihan Kepemimpinan Pengurus Salimah.</p>
+                    </div>
+
+                </div>
+
             </div>
 
-            <div class="service-card">
-                <h3>Struktur</h3>
-                <p>Susunan kepengurusan.</p>
-            </div>
+        </section>
+        
 
-            <div class="service-card">
-                <h3>Donasi</h3>
-                <p>Salurkan kontribusi sosial.</p>
+        <section class="berita">
+                <div class="berita-header">
+                    <h1>Berita</h1>
+                    <p>Publikasi terbaru dan agenda.</p>
+                </div>
+        </section>
+
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-brand">
+                <div class="footer-logo">
+                    <img src="https://th.bing.com/th/id/R.95983909802defd30381d6b50060ff9a?rik=R%2bXKFFXHJTUMng&riu=http%3a%2f%2f3.bp.blogspot.com%2f-UBh4T8jZDe0%2fTvx7q49SNjI%2fAAAAAAAAACE%2fyYbRy0lvoTc%2fs1600%2fLogo%2bbaru%2bsalimah.jpg&ehk=ZjXEWxAXKLzzfKmAO99f71fB39FIpuhIO7%2fHIK12fuA%3d&risl=&pid=ImgRaw&r=0" alt="Salimah Maluku">
+                </div>
+                <div>
+                    <h2>Salimah Maluku</h2>
+                    <p>DPW Maluku</p>
+                </div>
+            </div>
+            <div class="footer-sosmed">
+                <a href="https://www.instagram.com/pwsalimah.ambon?igsh=bjNmc2NqdGpwdHRq" target="_blank">
+                    <img src="https://static.vecteezy.com/system/resources/previews/018/930/692/original/instagram-logo-instagram-icon-transparent-free-png.png" alt="Instagram">
+                </a>
+                <a href="https://www.facebook.com/pwsalimah.ambon" target="_blank">
+                    <img src="https://cdn.pixabay.com/photo/2015/08/11/12/32/facebook-884378_1280.png" alt="Facebook">
+                </a>
             </div>
         </div>
-    </section>
+
+        <div class="footer-bottom">
+            <p>&copy; 2026 Salimah Maluku. All rights reserved.</p>
+        </div>
+
+    </footer>
 
 </body>
 </html>
